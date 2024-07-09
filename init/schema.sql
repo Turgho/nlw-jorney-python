ada5e8f0083ffbb2b64581dffc41da56b40f0935
Turgho <victor.hugo3692111@gmail.com>
2024-07-08 22:14:16 -0300

    config: starting project with init dir

---
+++ init/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS 'trips' (
+    id TEXT PRIMARY KEY,
+    description TEXT NOT NULL,
+    start_date DATETIME,
+    end_date DATETIME,
+    owner_name TEXT NOT NULL,
+    owner_email TEXT NO NULL,
+    status INTEGER, -- 1 para TRUE e 0 para FALSE
+);
+
+CREATE TABLE IF NOT EXISTS 'emails_to_invite' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT,
+    emails TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
+
+CREATE TABLE IF NOT EXISTS 'links_trip' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT,
+    link TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
